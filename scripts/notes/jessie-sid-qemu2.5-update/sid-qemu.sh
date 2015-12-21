a7388dbe29c94777f404af7823a5f73334c28ccd
the-snowwhite <producer@holotronic.dk>
2015-12-21 13:22:10 +0100

    Disabled script. Only for documentation of manual hand typed konsole emu  (jessie) --> emu2.5 (sid) package update process. Needs to be conveterd / changed to use apt / dpkg tools to be safe for any use.

--- scripts/notes/jessie-sid-qemu2.5-update/sid-qemu.sh
+++ scripts/notes/jessie-sid-qemu2.5-update/sid-qemu.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
 
-echo "Very unfinshed not usable in current state"
+echo "Scripted DEmo description of konsole commands used to update QEMU to 2.5 sid version om my exact Debian Jessie installation"
+echo "Not for generic use"
+
 exit
 
 set x
 mkdir -p qemu-down && cd qemu-down
 
 URL_LIST='[
+
 http://ftp.dk.debian.org/debian/pool/main/n/nettle/libnettle6_3.1.1-4_amd64.deb
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-utils_2.5+dfsg-1_amd64.deb
 http://ftp.dk.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.75_all.deb
 http://ftp.dk.debian.org/debian/pool/main/b/binfmt-support/binfmt-support_2.1.5-1_amd64.deb
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-user-static_2.5+dfsg-1_amd64.deb
 
+http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-x86_2.5+dfsg-1_amd64.deb
+
 http://ftp.dk.debian.org/debian/pool/main/libc/libcacard/libcacard0_2.5.0-2_amd64.deb
 
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libtinfo5_6.0+20151024-2_i386.deb
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libtinfo5_6.0+20151024-2_amd64.deb
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libtinfo5_6.0+20151024-2_armhf.deb
+
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libtinfo-dev_6.0+20151024-2_amd64.deb
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libtinfo-dev_6.0+20151024-2_armhf.deb
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libtinfo-dev_6.0+20151024-2_i386.deb
+
+http://ftp.dk.debian.org/debian/pool/main/x/xen/libxen-4.6_4.6.0-1_amd64.deb
+
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libncurses5-dev_6.0+20151024-2_amd64.deb
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libncurses5-dev_6.0+20151024-2_i386.deb
+http://ftp.dk.debian.org/debian/pool/main/n/ncurses/libncursesw5_6.0+20151024-2_amd64.deb
+
+http://ftp.dk.debian.org/debian/pool/main/s/slof/qemu-slof_20150429+dfsg-1_all.deb
+http://ftp.dk.debian.org/debian/pool/main/o/openbios/openbios-ppc_1.1+svn1340-1_all.deb
+http://ftp.dk.debian.org/debian/pool/main/o/openhackware/openhackware_0.4.1+git-20140423.c559da7c-2_all.deb
+
+wget http://ftp.dk.debian.org/debian/pool/main/o/openbios/openbios-sparc_1.1+svn1340-1_all.deb
+wget http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-sparc_2.5+dfsg-1_amd64.deb
+
+wget http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-misc_2.5+dfsg-1_amd64.deb
+
+http://ftp.dk.debian.org/debian/pool/main/s/spice/libspice-server-dev_0.12.6-4_amd64.deb
+
+http://ftp.dk.debian.org/debian/pool/main/o/openssl/libssl-dev_1.0.2e-1_amd64.deb
+http://ftp.dk.debian.org/debian/pool/main/o/openssl/libssl-dev_1.0.2e-1_i386.deb
 
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-arm_2.5+dfsg-1_amd64.deb
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-mips_2.5+dfsg-1_amd64.deb
@@ -23,20 +55,48 @@ http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-sparc_2.5+dfsg-1_ar
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-misc_2.5+dfsg-1_amd64.deb
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system-x86_2.5+dfsg-1_arm64.deb
 
-
-http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system_2.5+dfsg-1_amd64.deb
-
-http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu_2.5+dfsg-1_amd64.deb
-
 http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-user-binfmt_2.5+dfsg-1_amd64.deb
 
+
+http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-user_2.5+dfsg-1_amd64.deb
+http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu-system_2.5+dfsg-1_amd64.deb
+http://ftp.dk.debian.org/debian/pool/main/q/qemu/qemu_2.5+dfsg-1_amd64.deb
 
 http://ftp.dk.debian.org/debian/pool/main/s/schroot/schroot_1.6.10-2_amd64.deb
 ]'
 
-#wget -i $URL_LIST
+# uninstall / purge depedencies:
+
+sudo apt-get purge libncurses5-dev libncurses5* libncursesw5 libtinfo* libxen* libspice-server1* libssl1* libtinfo-dev 
+
+
+sudo dpkg -P qemu-*
+sudo dpkg -P qemu
+sudo dpkg -P qemu-user-static
+sudo dpkg -P qemu-system
+sudo dpkg -P qemu-utils
+sudo dpkg -P qemu-user-binfmt
+sudo dpkg -P qemu-system-ppc
+sudo dpkg -P libssl1.0.0_1.0.2d-1_amd64.deb libssl1.0.0_1.0.2d-1_i386.deb
+sudo dpkg -P libssl1 libssl
+sudo dpkg -P libssl1
+sudo dpkg -P libssl1.0.0
+sudo dpkg -P debootstrap
+sudo dpkg -P schroot
+sudo dpkg -P binfmt-support
+sudo dpkg -P schroot
+
+sudo apt-get autoremove
+sudo apt-get update
+sudo apt-get upgrade
+
+# download packets from sid repo:
+
+wget -i $URL_LIST
 URL_LIST2=($URL_LIST)
 filelist=''
+
+# create install packet name list:
 
 for file in "${URL_LIST2[@]}"
 do
@@ -46,7 +106,8 @@ do
     fi
 done
 
-#¤sudo dpkg -P ${filelist}
+# install packets
+sudo dpkg -P ${filelist}
 echo ${filelist}
 
 
